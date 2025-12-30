9242ad8c2a3998eac797c8b67da8fb5dc61074a9
Oleh <oleggulevic8@gmail.com>
2025-12-30 11:46:50 +0100

    made header and burger menu

--- Components/Header/BurrgerMenu.tsx
+++ Components/Header/BurrgerMenu.tsx
@@ -1,9 +1,16 @@
 ﻿import style from "./burger.module.css";
+import { RxCross1 } from "react-icons/rx";
+import type {Dispatch, SetStateAction} from "react";
 
-const BurrgerMenu = () => {
+
+
+const BurrgerMenu = ({isOpen,setIsOpen}:{isOpen:boolean, setIsOpen:Dispatch<SetStateAction<boolean>>}) => {
     return (
-        <div className={style.burger_menu}>
-            <img className={style.logo} src="../../public/logo.png" alt=""/>
+        <div className={`${style.burger_menu} ${isOpen?style.active:''}`}>
+                <RxCross1 className={style.cross} onClick={()=>setIsOpen(false)}/>
+                <div className={style.header}>
+                    <img className={style.logo} src="../../public/logo.png" alt=""/>
+                </div>
             <nav className={style.navbar}>
                 <div className={style.navlink}>Home</div>
                 <div className={style.navlink}>Explore</div>
--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -1,11 +1,13 @@
 ﻿import style from "./header.module.css"
 import BurrgerMenu from "./BurrgerMenu.tsx";
+import {useState} from "react";
+
 const Header = () => {
+    const [isOpen,setIsOpen] = useState(false);
     return (
         <header className={style.header}>
             <img className={style.logo} src="../../public/logo.png" alt=""/>
             <nav className={style.navbar}>
-
                     <div className={style.navlink}>Home</div>
                     <div className={style.navlink}>Explore</div>
                     <div className={style.navlink}>Rooms</div>
@@ -14,13 +16,13 @@ const Header = () => {
             </nav>
             <div className={style.button_block}>
                 <button className={style.login_button}>Log in</button>
-                <div className={style.burgger_menu}>
+                <div onClick={()=>setIsOpen(true)} className={style.burgger_menu}>
                     <div className={style.burgger_menu_item}></div>
                     <div className={style.burgger_menu_item}></div>
                     <div className={style.burgger_menu_item}></div>
                 </div>
             </div>
-            <BurrgerMenu/>
+            <BurrgerMenu isOpen={isOpen} setIsOpen={setIsOpen}/>
         </header>
     );
 };
